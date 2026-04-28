238fd6fe3268c535060c21cdfb510f147ea063c5
metadevv <164035951+themetadevv@users.noreply.github.com>
2026-04-28 22:57:59 +0545

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -26,7 +26,9 @@ project "imgui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+		"ImGuizmo.h",
+		"ImGuizmo.cpp"
 	}
 
 	filter "configurations:Debug"
