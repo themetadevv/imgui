db1426528a804aec8b01630546cf17399fdcbdac
metadevv <164035951+themetadevv@users.noreply.github.com>
2026-07-01 22:47:45 +0545

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,12 @@
-imgui_prj_output_dir = ("bin/imgui/builds")
-imgui_prj_intermediates_dir = ("bin/imgui/intermediates")
-
 project "imgui"
 	kind "StaticLib"
 	language "C++"
-    cppdialect "C++20"
+    cppdialect "C++23"
 	systemversion "latest"
 	staticruntime "Off" 
 
-	targetdir (imgui_prj_output_dir .. "/%{cfg.system}_%{cfg.buildcfg}")
-    objdir (imgui_prj_intermediates_dir .. "/%{cfg.system}")
+	targetdir ("bin/build/%{cfg.system}_%{cfg.buildcfg}")
+    objdir ("bin/intermediates/%{cfg.system}")
 
 	-- add required files
 	files
@@ -24,7 +21,9 @@ project "imgui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_glfw.cpp"
 	}
 
 	filter "configurations:Debug"
