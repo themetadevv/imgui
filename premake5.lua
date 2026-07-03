e88c969e7ed87330b607d98ed5359648ff64b181
metadevv <164035951+themetadevv@users.noreply.github.com>
2026-07-03 13:53:05 +0545

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -26,9 +26,7 @@ project "imgui"
         "ImGuizmo.cpp",
     
         "backends/imgui_impl_opengl3.h",
-        "backends/imgui_impl_opengl3.cpp",
-        "backends/imgui_impl_glfw.h",
-        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.cpp"
 	}
 
     includedirs {
